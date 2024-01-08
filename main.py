0c4d24ec61a961ba45c5b6eb5181aecfc5b2ead9
Alex Bowe <alexbowe@Bento.local>
2024-01-08 13:07:02 -0800

    restructuring

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-from routledge import load_routledge_chinese_word_frequency_data
+from pinlv.parsers.routledge import load_routledge_chinese_word_frequency_data
 
-# TODO: Make dataframes of arbitrary text file globs
+
 df = load_routledge_chinese_word_frequency_data()
-# print(df.dtypes)
 print(df.head(10))
 
 # TODO: Load the symbol frequencies
 # TODO: Load the radical database
+# TODO: Make dataframes of arbitrary text file globs
+# TODO: Add ability to adjust frequencies to prioritize learning song lyrics (or other media)
 # TODO: Translate my personal data and create a table to modify frequencies
 # TODO: Build a graph with radicals, hanzi, words, and sentences
 # TODO: Use pagerank to spread the ranks (compare the outcomes)
--- routledge/german.py
+++
@@ -1,6 +0,0 @@
-import os
-
-import pandas as pd
-
-DATA_DIR = "data"
-ROUTLEDGE_GERMAN_WORD_FREQUENCY_FILE = "routledge_german_word_frequency.txt"
